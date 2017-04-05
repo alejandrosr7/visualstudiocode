62ac8ebeb098af2cf4d853dd472968b3f151edf3
Alejandro Serna Rodriguez <alejocr7sr@gmail.com>
2017-04-05 16:35:38 -0500

    prueba

---
+++ CodigoFacilitoTutorial/CodigoFacilitoTutorial/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CodigoFacilitoTutorial
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			clsEmpleados clsEmpleado = new clsEmpleados();
+
+			clsEmpleado.Edad = 24;
+			clsEmpleado.Nombre = "Alejandro Serna Rodriguez";
+			clsEmpleado.SueldoMensual = 25.550m;
+
+			Console.WriteLine("El sueldo total es de: {0}", salarioTotal(clsEmpleado, diasTrabajados()));
+			Console.ReadKey();
+
+			/*Console.WriteLine("ingresa el primer número: ");
+			int numero1 = obtenerNumero();
+			Console.WriteLine("Ingresa el segundo número: ");
+			int numero2 = obtenerNumero();
+			compararNumeros(numero1, numero2);*/
+		}
+
+		static int diasTrabajados()
+		{
+			Console.WriteLine("Ingresa el número de días trabajados");
+			return Convert.ToInt32(Console.ReadLine());
+		}
+
+		static decimal salarioTotal(clsEmpleados empleado, int dias)
+		{
+			return empleado.calculaSalario(dias);
+		}
+
+		static void compararNumeros(int number1, int number2)
+		{
+			if (number1 > number2)
+			{
+				Console.WriteLine("El primer número es mayor");
+			}
+			else
+			{
+				Console.WriteLine("El segundo número es mayor");
+			}
+		}
+
+		static int obtenerNumero(){
+			return Convert.ToInt32(Console.ReadLine());
+		}
+}
+}
