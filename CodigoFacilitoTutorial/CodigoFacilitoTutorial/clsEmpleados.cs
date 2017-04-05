905c99bc1e54ec861da29866be98e5af76e9e7e2
Alejandro Serna Rodriguez <alejocr7sr@gmail.com>
2017-04-05 17:24:19 -0500

    getters and setters

---
+++ CodigoFacilitoTutorial/CodigoFacilitoTutorial/clsEmpleados.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace CodigoFacilitoTutorial
+{
+	public class clsEmpleados
+	{
+
+		public clsEmpleados(){
+			Nombre = "";
+			SueldoMensual = 0.0m;
+			Edad = 0;
+		}
+		private string _Nombre;
+		private decimal _SueldoMensual;
+		private int _Edad;
+
+		public string Nombre
+		{
+			get
+			{
+				return _Nombre;
+			}
+
+			set
+			{
+				_Nombre = value;
+			}
+		}
+
+		public decimal SueldoMensual
+		{
+			get
+			{
+				return _SueldoMensual;
+			}
+
+			set
+			{
+				_SueldoMensual = value;
+			}
+		}
+
+		public int Edad
+		{
+			get
+			{
+				return _Edad;
+			}
+
+			set
+			{
+				_Edad = value;
+			}
+		}
+
+		public decimal calculaSalario(int numeroDias) {
+			
+			return SueldoMensual * numeroDias;
+		}
+	}
+}
+
